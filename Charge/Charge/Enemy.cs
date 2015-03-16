f51a142e1cdcdfbd4fb1ac5d9c3406f75ec8a50d
ahr7ee <arosenrat@gmail.com>
2015-03-16 17:28:09 -0400

    added subclasses of the WorldEntity class

---
+++ Charge/Charge/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Charge
+{
+    //More methods and fields may be added later
+    class Enemy : WorldEntity
+    {
+        public Enemy(int x, int y, int width, int height) 
+        {
+            this.position = new Rectangle(x, y, width, height);
+        }
+    }
+}
